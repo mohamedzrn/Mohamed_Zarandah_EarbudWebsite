c09e37e3c17d39ca670407358de90c24b46a874a
mohamedzrn <mohammedzrn13@gmail.com>
2023-11-12 01:46:32 -0500

    added more content

---
+++ js/animation.js
@@ -0,0 +1,49 @@
+  (() => {
+    const canvas = document.querySelector("#explode-view");
+    const context = canvas.getContext("2d");
+    canvas.width = 1920;
+    canvas.height = 1080;
+    const frameCount = 268; // how many still frames do we have
+    const images = []; // an array to hold all of our images
+    //create an object literal with a property frame to hold the current frame
+    const buds = {
+        frame: 0
+    };
+
+    for (let i=0; i<frameCount; i++) {
+        //different ways to do it:
+        //console.log(i);
+        //const img = new Image();
+        const img = document.createElement("img");
+        // need to recreate a string: images/scrolling-render_001.jpg
+        img.src = `images/explode_${(i+1).toString().padStart(4,'0')}.jpg`;
+        images.push(img);
+
+    }
+
+    //console.log(images)
+    //Not actually animating a DOM element, but rather an object
+    //which contains a frame count
+    gsap.to(buds, {
+        frame: 257,
+        snap: "frame",
+        scrollTrigger: {
+            trigger: "#explode-view",
+            pin: true,
+            scrub: 1,
+            start: "top top"
+        },
+        onUpdate: render
+    })
+
+    images[0].addEventListener("onload", render);
+
+    function render() {
+        //console.log(buds.frame);
+        //console.log(images[buds.frame]);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(images[buds.frame], 0, 0);
+    }
+
+
+})();
